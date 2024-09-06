02d6bc1cf76481e8c2e43886938e64765110bf72
adigoj <adam.alperk@gmail.com>
2024-09-06 18:35:22 +0530

    Sliding functionality implemented for listings

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
   const sliderCard = document.querySelector(".slider-card");
   const slides = document.querySelectorAll(".slide");
-  const dots = document.querySelectorAll(".dot"); // Select all dots
+  const listingsCard = document.querySelector(".listings-card");
+  const listingImages = document.querySelectorAll(".listings-image-container"); // Select all listings images
+  const dots = document.querySelectorAll(".dot");
+  const listDots = document.querySelectorAll(".list-dot");
   const totalSlides = slides.length;
+  const totalListings = listingImages.length;
   const adjuster = ((totalSlides - 1) * 100) / 2;
+  const listingAdjuster = ((totalListings - 1) * 30) / 2;
   let currentIndex = 0;
+  let listingIndex = 0;
 
   sliderCard.style.transform = `translateX(${adjuster}vw)`;
+  listingsCard.style.transform = `translateX(${listingAdjuster}vw)`;
 
   function updateSlidePosition() {
     const offset = -currentIndex * 100 + adjuster;
@@ -14,9 +21,21 @@ document.addEventListener("DOMContentLoaded", function () {
     updateDots(); // Update dots each time the slide position changes
   }
 
+  function updateListingsPosition() {
+    const listOffset = -listingIndex * 30 + listingAdjuster; // Adjust the width if necessary
+    listingsCard.style.transform = `translateX(${listOffset}vw)`;
+    updateListDots();
+  }
+
   function updateDots() {
     dots.forEach((dot, index) => {
       dot.classList.toggle("active", index === currentIndex);
+    });
+  }
+
+  function updateListDots() {
+    listDots.forEach((listdot, index) => {
+      listdot.classList.toggle("active", index === listingIndex);
     });
   }
 
@@ -28,6 +47,11 @@ document.addEventListener("DOMContentLoaded", function () {
   function showPreviousSlide() {
     currentIndex = (currentIndex - 1 + totalSlides) % totalSlides;
     updateSlidePosition();
+  }
+
+  function showNextListing() {
+    listingIndex = (listingIndex + 1) % totalListings;
+    updateListingsPosition();
   }
 
   // Attach event listeners to the arrows
@@ -49,9 +73,18 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 
+  listDots.forEach((listdot, index) => {
+    listdot.addEventListener("click", () => {
+      listingIndex = index;
+      updateListingsPosition();
+    });
+  });
+
   // Optional: Auto-slide functionality
   setInterval(showNextSlide, 5000); // Change slide every 5 seconds
+  setInterval(showNextListing, 5000);
 
   // Initialize dots
   updateDots();
+  updateListDots();
 });
